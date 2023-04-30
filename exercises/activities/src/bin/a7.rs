fb9a8fc65cf42df4f9b45fd8c5b8755c96ccee86
ShivamBhadani <sbhadani882@outlook.com>
2023-04-30 10:41:39 +0530

    a7

--- exercises/activities/src/bin/a7.rs
+++ exercises/activities/src/bin/a7.rs
@@ -5,9 +5,26 @@
 //
 // Notes:
 // * Use an enum with color names as variants
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
 // * Use a function to print the color name
 // * The function must use the enum as a parameter
+fn print_color_name(my_color: Color) {
+
 // * Use a match expression to determine which color
 //   name to print
+match my_color {
+    Color::Red => println!("Red"),
+    Color::Green => println!("Green"),
+    Color::Blue => println!("Blue"),
+}
+}
 
-fn main() {}
+fn main() {
+    print_color_name(Color::Red);
+    print_color_name(Color::Green);
+    print_color_name(Color::Blue);
+}
