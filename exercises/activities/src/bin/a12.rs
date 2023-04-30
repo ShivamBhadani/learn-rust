bc292e2fb4a62c8422a3117a800bcfafa36ccafb
ShivamBhadani <sbhadani882@outlook.com>
2023-04-30 20:07:18 +0530

    a12

--- exercises/activities/src/bin/a12.rs
+++ exercises/activities/src/bin/a12.rs
@@ -9,5 +9,42 @@
 // * Use an enum for the box color
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
-
-fn main() {}
+enum Color {
+    Brown,
+    Black,
+    White,
+}
+impl Color{
+    fn print(&self){
+        match self {
+            Color::Brown => println!("Brown"),
+            Color::Black => println!("Black"),
+            Color::White => println!("White"),
+        }
+    }
+}
+struct ShippingBox {
+    dimensions: (i32, i32, i32),
+    weight: i32,
+    color: Color,
+}
+impl ShippingBox {
+    fn new(dimensions: (i32, i32, i32), weight: i32, color: Color) -> Self {
+        Self {
+            dimensions,
+            weight,
+            color,
+        }
+    }
+    fn print(&self) {
+        println!(
+            "Dimensions: {:?}, Weight: {}, Color: ",
+            self.dimensions, self.weight
+        );
+        self.color.print();
+    }
+}
+fn main() {
+    let box1 = ShippingBox::new((1, 2, 3), 4, Color::Brown);
+    box1.print();
+}
