bfd163749adca1474ea585bd0676c7e0c67e3ede
ShivamBhadani <sbhadani882@outlook.com>
2023-05-08 02:54:47 +0530

    a15

--- exercises/activities/src/bin/a15.rs
+++ exercises/activities/src/bin/a15.rs
@@ -11,4 +11,37 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+enum Ticket {
+    Backstage(String, f32),
+    Vip(String, f32),
+    Standard(f32),
+}
+
+impl Ticket {
+    fn print(&self) {
+        match self {
+            Ticket::Backstage(name, price) => {
+                println!("Backstage ticket for {} costs ${}", name, price);
+            }
+            Ticket::Vip(name, price) => {
+                println!("VIP ticket for {} costs ${}", name, price);
+            }
+            Ticket::Standard(price) => {
+                println!("Standard ticket costs ${}", price);
+            }
+        }
+    }
+}
+
+fn main() {
+    let tickets = vec![
+        Ticket::Backstage("John".to_string(), 100.0),
+        Ticket::Vip("Jane".to_string(), 200.0),
+        Ticket::Standard(50.0),
+    ];
+
+    for ticket in tickets {
+        ticket.print();
+    }
+    
+}
